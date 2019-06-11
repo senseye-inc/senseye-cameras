b74acd6c4166e8edc24bb3ad9d21519e8d13d08c
Allen Zhang <allen.zhang@senseye.co>
2019-06-11 16:50:36 +0000

    Merged in feature/revamp-camera-handler (pull request #25)
    rewrote camera handler from whatever complicated bullshit it was earlier

--- senseye_cameras/camera_handler.py
+++ senseye_cameras/camera_handler.py
@@ -1,8 +1,8 @@
 import logging
 
-from senseye_utils import LoopThread
-from . camera_reader import CameraReader
-from . camera_writer import CameraWriter
+from senseye_utils import LoopThread, RapidEvents
+from . cameras.camera_factory import create_camera
+from . recorders.recorder_factory import create_recorder
 
 log = logging.getLogger(__name__)
 
@@ -24,70 +24,55 @@ class CameraHandler(LoopThread):
         recorder_config (str)
         path (str): file frames are written to.
     '''
-    def __init__(self, camera_feed=None, viewer=False,
+    def __init__(self, camera_feed=None,
         camera_type='usb', camera_config={}, camera_id=0,
         recorder_type='raw', recorder_config={}, path=None,
     ):
         self.camera_feed = camera_feed
-        self.viewer = viewer
-        self.camera_type = camera_type
-        self.camera_config = camera_config
-        self.camera_id = camera_id
-        self.recorder_type = recorder_type
-        self.recorder_config = recorder_config
-        self.path = path
 
-        self.reader = None
-        self.writer = None
+        self.camera = create_camera(camera_type=camera_type, config=camera_config, id=camera_id)
+        self.recorder = create_recorder(recorder_type=recorder_type, path=path, config=recorder_config)
+
+        self.reading = False
+        self.writing = False
+
+        self.re = RapidEvents(f'camera_handler:{camera_type}:{camera_id}:{recorder_type}')
 
         LoopThread.__init__(self, frequency=camera_config.get('fps', -1))
 
-    def initialize_writer(self):
-        if self.writer is None:
-            self.writer = CameraWriter(
-                camera_feed=self.camera_feed,
-                recorder_type=self.recorder_type,
-                recorder_config=self.recorder_config,
-                path=self.path,
-            )
+    def set_path(self, path=None):
+        log.info(f'Recorder path set to {path}')
+        self.recorder.set_path(path)
 
-    def initialize_reader(self):
-        if self.reader is None:
-            self.reader = CameraReader(
-                camera_feed=self.camera_feed,
-                camera_type=self.camera_type,
-                camera_config=self.camera_config,
-                camera_id=self.camera_id,
-            )
+    def start_reading(self):
+        log.info(f'Camera reading.')
+        self.camera.open()
+        self.reading = True
 
-    def start_reader(self):
-        self.initialize_reader()
-        self.reader.on_start()
+    def start_writing(self):
+        log.info('Recorder writing.')
+        self.writing = True
 
-    def start_writer(self):
-        self.initialize_writer()
+    def stop_reading(self):
+        log.info(f'Camera closing.')
+        self.reading = False
+        if self.camera:
+            self.camera.close()
 
-    def stop_reader(self):
-        if self.reader:
-            self.reader.stop()
-        self.reader = None
-
-    def stop_writer(self):
-        self.writer.on_stop()
-        self.writer = None
+    def stop_writing(self):
+        log.info('Recorder closing.')
+        self.writing = False
+        if self.recorder:
+            self.recorder.close()
 
     def on_stop(self):
-        self.stop_reader()
-        self.stop_writer()
-
-    def set_path(self, path=None):
-        self.initialize_writer()
-        self.writer.set_path(path=path)
+        self.stop_reading()
+        self.start_writing()
 
     def loop(self):
-        if self.reader:
-            frame, timestamp = self.reader.camera.read()
+        if self.reading:
+            frame, timestamp = self.camera.read()
             if frame is not None:
-                self.reader.re.publish(self.reader.camera_feed, frame=frame, timestamp=timestamp)
-                if self.writer is not None:
-                    self.writer.recorder.write(frame)
+                self.re.publish(self.camera_feed, frame=frame, timestamp=timestamp)
+                if self.writing:
+                    self.recorder.write(frame)
