b377b07f89de7bbf478bfabd1d077cd3522708e3
Allen Zhang <allen.zhang@senseye.co>
2019-06-14 21:34:01 +0000

    Merged in bugfix/test-fixes (pull request #39)
    Bugfix/test fixes

--- tests/camera_factory_test.py
+++ tests/camera_factory_test.py
@@ -9,11 +9,11 @@ def test_create_usb_camera():
     cam = create_input(type='usb', id=USB_VIDEO)
     cam.open()
 
-    nt.assert_true(cam.camera.isOpened())
+    nt.assert_true(cam.input.isOpened())
 
     frame, timestamp = cam.read()
     nt.assert_is_not_none(frame)
 
 
     cam.close()
-    nt.assert_is_none(cam.camera)
+    nt.assert_is_none(cam.input)
