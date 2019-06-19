6a23db18e4067f2acaf6c0b60e92ae6a059edebd
Allen Zhang <allen.zhang@senseye.co>
2019-06-19 20:43:58 +0000

    Merged in feature/audio-windows (pull request #44)
    Ffmpeg audio working on windows.

--- senseye_cameras/output/audio_ffmpeg.py
+++ senseye_cameras/output/audio_ffmpeg.py
@@ -26,11 +26,10 @@ class AudioFfmpeg(Output):
 
     def generate_cmd(self):
         if os.name == 'posix':
-            self.cmd = f'ffmpeg -f avfoundation -hide_banner -loglevel warning -y -i ":0" {self.tmp_path}'
+            self.cmd = f'ffmpeg -f avfoundation -hide_banner -loglevel warning -y -i :0 {self.tmp_path}'.split()
         else:
             audio_device = self.get_first_dshow_audio_device()
-            self.cmd = f'ffmpeg -f dshow -i audio={audio_device} {self.tmp_path}'
-
+            self.cmd = f'ffmpeg -f dshow -y -i audio={audio_device} {self.tmp_path}'
 
     def get_first_dshow_audio_device(self):
         # get ffmpeg list device output
@@ -51,22 +50,21 @@ class AudioFfmpeg(Output):
         return ''
 
     def open(self):
-        self.process = Popen(self.cmd, shell=True)
+        self.close()
+        self.process = Popen(self.cmd, stdin=PIPE)
+        log.info(f"{self.__class__.__name__} recording started.")
 
     def close(self):
         '''
         Closes file handle.
         Renames tmp file.
 
-        When running an ffmpeg command that writes directly to a file,
-        the subprocess closes on a SIGINT.
+        Sends a 'q' key input to the ffmpeg process which quits it.
         '''
         if self.process:
             if self.process.poll() == None:
-                if platform.system() == 'Windows':
-                    os.kill(signal.CTRL_C_EVENT, 0)
-                if platform.system() == 'Darwin' or platform.system() == 'Linux':
-                    self.process.send_signal(signal.SIGINT)
+                # quit the ffmpeg process by sending a q bytestring.
+                self.process.communicate(b'q')
             Output.close(self)
 
         self.process = None
